01d106096c776ef3d6d01953817dfc6d2f57f3d1
Jerome de Dios <jeromededios.de@proton.me>
2025-04-11 06:43:21 +0200

    test: add llm rag evaluation

--- tests/test_llm_rag.py
+++ tests/test_llm_rag.py
@@ -1,8 +1,189 @@
-import pytest
-from langsmith import evaluate
+import os
+from datetime import datetime
 
-from app.evaluation.evaluator import target
-from app.evaluation.evaluator_rag import dataset_name, correctness, groundedness, relevance, retrieval_relevance, client
+from langchain_openai import ChatOpenAI
+from langsmith import traceable
+
+from app.evaluation.prompt import correctness_instructions, relevance_instructions, grounded_instructions, \
+    retrieval_relevance_instructions
+from app.utility.env import get_env_key, get_open_ai_model
+from app.utility.rag_qa import rag_evaluation
+
+from langsmith import Client
+
+from typing_extensions import Annotated, TypedDict
+
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["LANGSMITH_API_KEY"] = get_env_key("LANGSMITH_API_KEY")
+os.environ["OPENAI_API_KEY"] = get_env_key("OPEN_AI_KEY")
+
+llm = ChatOpenAI(
+    model=get_open_ai_model(),
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2
+)
+
+
+@traceable()
+def rag_bot(question: str) -> dict:
+    retriever = rag_evaluation()
+    docs = retriever.invoke(question)
+    docs_string = "".join(doc.page_content for doc in docs)
+    instructions = f"""
+        You are a helpful assistant who is good at analyzing source information and answering questions.
+
+        Use the following source documents to answer the user's questions.
+
+        If you don't know the answer, just say that you don't know.
+
+        Use three sentences maximum and keep the answer concise.
+
+        Documents: {docs_string}"""
+
+    # langchain ChatModel will be automatically traced
+    ai_msg = llm.invoke([
+        {"role": "system", "content": instructions},
+        {"role": "user", "content": question},
+    ],
+    )
+
+    return {"answer": ai_msg.content, "documents": docs}
+
+
+answer_from_rag = """
+Inflammatory medicines, particularly those classified as anti-inflammatory and antipruritic, include:
+
+1. **Betamethasone**: A potent corticosteroid used in cream or ointment form (0.1% as valerate).
+2. **Hydrocortisone**: A weaker corticosteroid available in cream or ointment form (1% acetate).
+3. **Calamine**: Often used in lotion form for its soothing properties.
+
+These medications are commonly used to reduce inflammation and alleviate itching associated with various skin conditions.
+"""
+
+examples = [
+    {
+        "inputs": {"question": "What are the inflammatory medicines?"},
+        "outputs": {"answer": f"{answer_from_rag}"},
+    }
+]
+
+client = Client()
+
+# Create the dataset and examples in LangSmith
+dataset_name = "Nurse Assistant Dataset - " + str(datetime.now())
+dataset = client.create_dataset(dataset_name=dataset_name)
+client.create_examples(
+    dataset_id=dataset.id,
+    examples=examples
+)
+
+
+# Grade output schema
+class CorrectnessGrade(TypedDict):
+    explanation: Annotated[str, ..., "Explain your reasoning for the score"]
+    correct: Annotated[bool, ..., "True if the answer is correct, False otherwise."]
+
+
+# Grader LLM
+grader_llm = (ChatOpenAI(model=get_open_ai_model(), temperature=0)
+              .with_structured_output(CorrectnessGrade,
+                                      method="json_schema",
+                                      strict=True))
+
+
+def correctness(inputs: dict, outputs: dict, reference_outputs: dict) -> bool:
+    """An evaluator for RAG answer accuracy"""
+    answers = f"""
+                    QUESTION: {inputs['question']}
+                    GROUND TRUTH ANSWER: {reference_outputs['answer']}
+                    STUDENT ANSWER: {outputs['answer']}
+               """
+    # Run evaluator
+    grade = grader_llm.invoke([
+        {"role": "system", "content": correctness_instructions},
+        {"role": "user", "content": answers}
+    ])
+    return grade["correct"]
+
+
+# Grade output schema
+class RelevanceGrade(TypedDict):
+    explanation: Annotated[str, ..., "Explain your reasoning for the score"]
+    relevant: Annotated[bool, ..., "Provide the score on whether the answer addresses the question"]
+
+
+# Grader LLM
+relevance_llm = (ChatOpenAI(model=get_open_ai_model(), temperature=0)
+                 .with_structured_output(RelevanceGrade,
+                                         method="json_schema",
+                                         strict=True))
+
+
+# Evaluator
+def relevance(inputs: dict, outputs: dict) -> bool:
+    """A simple evaluator for RAG answer helpfulness."""
+    answer = f"QUESTION: {inputs['question']}\nSTUDENT ANSWER: {outputs['answer']}"
+
+    grade = relevance_llm.invoke([
+        {"role": "system", "content": relevance_instructions},
+        {"role": "user", "content": answer}
+    ])
+    return grade["relevant"]
+
+
+# Grade output schema
+class GroundedGrade(TypedDict):
+    explanation: Annotated[str, ..., "Explain your reasoning for the score"]
+    grounded: Annotated[bool, ..., "Provide the score on if the answer hallucinates from the documents"]
+
+
+# Grader LLM
+grounded_llm = (ChatOpenAI(model=get_open_ai_model(), temperature=0)
+                .with_structured_output(GroundedGrade,
+                                        method="json_schema",
+                                        strict=True))
+
+
+# Evaluator
+def groundedness(inputs: dict, outputs: dict) -> bool:
+    """A simple evaluator for RAG answer groundedness."""
+    doc_string = "\n\n".join(doc.page_content for doc in outputs["documents"])
+    answer = f"FACTS: {doc_string}\nSTUDENT ANSWER: {outputs['answer']}"
+    grade = grounded_llm.invoke(
+        [{"role": "system", "content": grounded_instructions}, {"role": "user", "content": answer}])
+    return grade["grounded"]
+
+
+# Grade output schema
+class RetrievalRelevanceGrade(TypedDict):
+    explanation: Annotated[str, ..., "Explain your reasoning for the score"]
+    relevant: Annotated[bool, ..., "True if the retrieved documents are relevant to the question, False otherwise"]
+
+
+# Grader LLM
+retrieval_relevance_llm = (ChatOpenAI(model=get_open_ai_model(), temperature=0)
+                           .with_structured_output(RetrievalRelevanceGrade,
+                                                   method="json_schema",
+                                                   strict=True))
+
+
+def retrieval_relevance(inputs: dict, outputs: dict) -> bool:
+    """An evaluator for document relevance"""
+    doc_string = "\n\n".join(doc.page_content for doc in outputs["documents"])
+    answer = f"FACTS: {doc_string}\nQUESTION: {inputs['question']}"
+
+    # Run evaluator
+    grade = retrieval_relevance_llm.invoke([
+        {"role": "system", "content": retrieval_relevance_instructions},
+        {"role": "user", "content": answer}
+    ])
+    return grade["relevant"]
+
+
+def target(inputs: dict) -> dict:
+    return rag_bot(inputs["question"])
 
 
 class TestLLMRAG:
